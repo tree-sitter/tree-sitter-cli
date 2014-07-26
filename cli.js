ddad9ae5aa8a2351e2cf5d5273cfe906a4913537
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-07-26 11:41:00 -0700

    Get CLI compile command working

--- cli.js
+++ cli.js
@@ -6,21 +6,39 @@ var treeSitter = require("./index"),
     templates = require("./lib/templates"),
     fs = require("fs"),
     path = require("path"),
+    mkdirp = require("mkdirp"),
     cp = require("ncp"),
-    cwd = process.cwd();
+    cwd = process.cwd(),
+    command = process.argv[2];
 
-var grammarHash = require(path.join(cwd, "grammar"));
-var grammar = treeSitter.grammar(grammarHash);
+switch (command) {
+  case "compile":
+    compile();
+    break;
+  case undefined:
+    console.log("Usage: %s compile", path.basename(process.argv[1]));
+    break;
+  default:
+    console.log("Unrecognized command '%s'", command);
+    break;
+}
 
-fs.writeFileSync(path.join(cwd, "parser.c"), treeSitter.compile(grammar));
-fs.writeFileSync(path.join(cwd, "parser.cc"), templates.code(grammar.name));
-fs.writeFileSync(path.join(cwd, "binding.gyp"), templates.gyp(grammar.name));
+function compile() {
+  var grammarHash = require(path.join(cwd, "grammar")),
+      grammar = treeSitter.grammar(grammarHash);
 
-cp(
-  path.join(__dirname, "vendor", "tree-sitter", "include"),
-  path.join(cwd, "include"),
-  function(err) {
-    if (err) {
-      console.log("Error: ", err);
-    }
-  });
+  mkdirp.sync(path.join(cwd, "src"));
+  fs.writeFileSync(path.join(cwd, "src", "parser.c"), treeSitter.compile(grammar));
+  fs.writeFileSync(path.join(cwd, "src", "init.cc"), templates.initCC(grammar.name));
+  fs.writeFileSync(path.join(cwd, "binding.gyp"), templates.bindingGyp(grammar.name));
+  fs.writeFileSync(path.join(cwd, "index.js"), templates.indexJS(grammar.name));
+
+  cp(
+    path.join(__dirname, "vendor", "tree-sitter", "include"),
+    path.join(cwd, "include"),
+    function(err) {
+      if (err) {
+        console.log("Error: ", err);
+      }
+    });
+}
