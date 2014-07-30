23710157915e5feafdac65e0d0b313462fb1cfcc
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-07-30 00:37:45 -0700

    Add test command to CLI
    Also, reorganize source files, so the lib directory contains two
    children - api and cli.

--- cli.js
+++ cli.js
@@ -1,44 +1,23 @@
 #!/usr/bin/env node
 
-require("coffee-script/register");
-
-var treeSitter = require("./index"),
-    templates = require("./lib/templates"),
-    fs = require("fs"),
+var cli = require("./lib/cli"),
     path = require("path"),
-    mkdirp = require("mkdirp"),
-    cp = require("ncp"),
-    cwd = process.cwd(),
     command = process.argv[2];
 
 switch (command) {
   case "compile":
-    compile();
+    cli.compile();
     break;
+
+  case "test":
+    cli.test(process.exit);
+    break;
+
   case undefined:
-    console.log("Usage: %s compile", path.basename(process.argv[1]));
+    console.log("Usage: %s <compile|test>", path.basename(process.argv[1]));
     break;
+
   default:
     console.log("Unrecognized command '%s'", command);
     break;
-}
-
-function compile() {
-  var grammarHash = require(path.join(cwd, "grammar")),
-      grammar = treeSitter.grammar(grammarHash);
-
-  mkdirp.sync(path.join(cwd, "src"));
-  fs.writeFileSync(path.join(cwd, "src", "parser.c"), treeSitter.compile(grammar));
-  fs.writeFileSync(path.join(cwd, "src", "binding.cc"), templates.bindingCC(grammar.name));
-  fs.writeFileSync(path.join(cwd, "binding.gyp"), templates.bindingGyp(grammar.name));
-  fs.writeFileSync(path.join(cwd, "index.js"), templates.indexJS(grammar.name));
-
-  cp(
-    path.join(__dirname, "vendor", "tree-sitter", "include"),
-    path.join(cwd, "include"),
-    function(err) {
-      if (err) {
-        console.log("Error: ", err);
-      }
-    });
 }
--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-module.exports = {
-  compile: require("./lib/binding").compile,
-  compileAndLoad: require("./lib/compile_and_load"),
-  grammar: require("./lib/grammar"),
-  rules: require("./lib/rules")
-};
+module.exports = require("./lib/api");
