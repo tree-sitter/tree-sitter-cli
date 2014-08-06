ef5692a6ca1260c1157faf10dc51785d59b0be25
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-08-06 00:55:58 -0700

    Remove execSync dependency

--- src/language.cc
+++ src/language.cc
@@ -2,6 +2,8 @@
 #include <uv.h>
 #include <string>
 #include "tree_sitter/runtime.h"
+#include <unistd.h>
+#include <sys/wait.h> 
 
 using namespace v8;
 
@@ -9,16 +11,71 @@ namespace node_tree_sitter_compiler {
 
 static Persistent<Function> constructor;
 
+static std::string run_cmd(const char *cmd, const char *args[]) {
+  int child_pid = fork();
+  if (child_pid == 0) {
+    close(0);
+    dup2(1, 0);
+    dup2(2, 1);
+    dup2(1, 2);
+    execvp(cmd, (char * const * )args);
+    return "";
+  } else if (child_pid > 0) {
+    int status;
+    waitpid(child_pid, &status, 0);
+    if (WIFEXITED(status)) {
+      if (WEXITSTATUS(status) == 0)
+        return "";
+      else
+        return "command failed";
+    } else {
+      return "child process did not exit";
+    }
+  } else {
+    return "fork failed";
+  }
+}
+
 Handle<Value> LoadLanguage(const Arguments &args) {
   HandleScope scope;
 
-  Handle<String> js_filename = Handle<String>::Cast(args[0]);
+  Handle<String> js_src_filename = Handle<String>::Cast(args[0]);
   Handle<String> js_language_name = Handle<String>::Cast(args[1]);
-  String::Utf8Value filename(js_filename);
-  String::Utf8Value language_name(js_language_name);
+  Handle<String> js_header_dir = Handle<String>::Cast(args[2]);
+  std::string src_filename(*String::Utf8Value(js_src_filename));
+  std::string language_name(*String::Utf8Value(js_language_name));
+  std::string header_dir(*String::Utf8Value(js_header_dir));
+  std::string obj_filename(src_filename + ".o");
+  std::string lib_filename(src_filename + ".so");
+
+  std::string error = run_cmd("gcc", (const char *[]){
+    "gcc",
+    "-x", "c",
+    "-fPIC",
+    "-I", header_dir.c_str(),
+    "-c", src_filename.c_str(),
+    "-o", obj_filename.c_str(),
+    NULL
+  });
+  if (!error.empty()) {
+    ThrowException(Exception::Error(String::New(("Failed to compile C code - " + error).c_str())));
+    return scope.Close(Undefined());
+  }
+
+  error = run_cmd("gcc", (const char *[]){
+    "gcc",
+    "-shared",
+    "-Wl", obj_filename.c_str(),
+    "-o", lib_filename.c_str(),
+    NULL
+  });
+  if (!error.empty()) {
+    ThrowException(Exception::Error(String::New(("Failed to link C code" + error).c_str())));
+    return scope.Close(Undefined());
+  }
 
   uv_lib_t parser_lib;
-  int error_code = uv_dlopen(*filename, &parser_lib);
+  int error_code = uv_dlopen(lib_filename.c_str(), &parser_lib);
   if (error_code) {
     Handle<String> message = String::New(uv_dlerror(&parser_lib));
     ThrowException(Exception::Error(
@@ -27,7 +84,7 @@ Handle<Value> LoadLanguage(const Arguments &args) {
   }
 
   const TSLanguage * (* language_fn)() = NULL;
-  error_code = uv_dlsym(&parser_lib, (std::string("ts_language_") + *language_name).c_str(), (void **)&language_fn);
+  error_code = uv_dlsym(&parser_lib, (std::string("ts_language_") + language_name).c_str(), (void **)&language_fn);
   if (error_code) {
     Handle<String> message = String::New(uv_dlerror(&parser_lib));
     ThrowException(Exception::Error(
