809ae0b330c528af59c299b45e3494291adf2c6a
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-08-24 01:08:28 -0700

    Use nvm in project file

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -9,8 +9,7 @@ flags = [
     '-stdlib=libc++',
     '-x', 'c++',
     '-I', 'src',
-    '-I', 'node_modules/tree-sitter/include',
-    '-I', '/usr/local/include/node',
+    '-I', os.path.expanduser('~/.nvm/current/include/node'),
     '-I', 'vendor/tree-sitter/include',
     '-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/c++/v1',
 ]
