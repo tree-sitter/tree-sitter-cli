f246976d6cc90f8e529696f23bf09825af465425
Max Brunsfeld <maxbrunsfeld@gmail.com>
2016-01-13 21:27:31 -0800

    Use the error_type field to check for errors

--- src/compile.cc
+++ src/compile.cc
@@ -9,7 +9,7 @@ NAN_METHOD(Compile) {
   String::Utf8Value grammar_json(info[0]);
   TSCompileResult compile_result = ts_compile_grammar(*grammar_json);
 
-  if (compile_result.error_message) {
+  if (compile_result.error_type != TSCompileErrorTypeNone) {
     Local<Value> error = Nan::Error(compile_result.error_message);
     Local<Object>::Cast(error)->Set(Nan::New("isGrammarError").ToLocalChecked(), Nan::True());
     Nan::ThrowError(error);
